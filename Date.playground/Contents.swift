6542abc1767d5108b98f0ec9e4768d7901595305
Alexlion2308 <alexandreleconquerant@gmail.com>
2016-03-21 00:08:03 +0100

    Playground

---
+++ Date.playground/Contents.swift
@@ -0,0 +1,40 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+/* var date = NSDate(timeIntervalSince1970: 3600)
+var date1 = NSDate(timeIntervalSince1970: 3700)
+
+var dateFormateur : NSDateFormatter = NSDateFormatter()
+dateFormateur.dateFormat = "HH:mm"
+let lestring: String = dateFormateur.stringFromDate(date1)
+print(lestring)
+func isBefore(date1: NSDate,date2: NSDate) -> Bool {
+    if date2.compare(date1).rawValue == 1 {
+        return true }
+    else {
+        return false
+    }}
+
+var tableau = [NSDate]()
+tableau.append(NSDate(timeIntervalSince1970: 200))
+tableau.append(NSDate(timeIntervalSince1970: 800))
+tableau.append(NSDate(timeIntervalSince1970: 500))
+tableau.append(NSDate(timeIntervalSince1970: 200))
+tableau.append(NSDate(timeIntervalSince1970: 400))
+tableau.append(NSDate(timeIntervalSince1970: 1300))
+tableau.append(NSDate(timeIntervalSince1970: 100))
+tableau.append(NSDate(timeIntervalSince1970: 2800))
+tableau.append(NSDate(timeIntervalSince1970: 250))
+tableau.append(NSDate(timeIntervalSince1970: 480))
+tableau.append(NSDate(timeIntervalSince1970: 960))
+tableau.append(NSDate(timeIntervalSince1970: 3600))
+tableau.append(NSDate(timeIntervalSince1970: 600))
+tableau.sortInPlace({isBefore($0, date2: $1)})
+tableau */
+
+var t : NSNumber = 1
+var i : Int = 3 + t.integerValue
+print(i)
